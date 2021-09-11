307ffd88556c9c9f12cb4146fae363f1a34ba1b2
Vincent <tazuku.66@gmail.com>
2021-09-11 10:56:13 +0200

    Menu burger for nav bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import {IoMdMenu} from "react-icons/io"
+
 const NavBar = () => {
+
+    //Affiche la nav bar et change la couleur de l'icon
+    function showNav () {
+        const navBar = document.querySelector('nav ol')
+        const navBurger = document.querySelector('nav svg')
+        if(navBar.className === ""){
+            navBar.classList.add('d-flex')
+            navBurger.style.color = 'white'
+        } else {
+            navBar.classList.remove('d-flex')
+            navBurger.style.color = '#24376f'
+        }
+    }
+
     return (
         <nav>
+            <IoMdMenu onClick={showNav}/>
             <ol>
-                <Link to={'/'}>Paris Events</Link>
+                <Link to={'/'}>PARIS EVENTS</Link>
                 <Link to={'/'}>Accueil</Link>
-                <Link to={'/liste-des-evenements'}>Liste des événements</Link>
+                <Link to={'/liste-des-evenements'}>Recherche</Link>
                 <Link to={"/favoris"}>Favoris</Link>
             </ol>
         </nav>
