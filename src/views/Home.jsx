d1a1520bdaeb49a784917ef7ebd5bcf1a41abb34
Vincent <tazuku.66@gmail.com>
2021-09-12 16:14:26 +0200

    Loader durant le fetch et gestion en cas d'aucune donnée

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,24 +2,35 @@ import React, {useEffect, useState} from 'react';
 import eventService from "../services/eventService";
 import Card from "../components/Card/Card";
 import Title from "../components/Title/Title";
+import Loader from "react-loader-spinner";
 
 const Home = () => {
     /* Stockage des données de l'API */
-    const [eventArray, setEventArray] =  useState([])
+    const [eventArray, setEventArray] = useState([])
+    const [loading, setloading] = useState(" ")
 
     /* Lors du chargement de la page injecte les données de l'API dans la variable eventArray */
     useEffect(() => {
+        setloading(false)
         eventService
             .fetchLastUpadted()
-            .then((data) => setEventArray(data.records))
+            .then((data) => {
+                setEventArray(data.records)
+                setloading(true)
+            })
     }, [])
 
     return (
         <div id={"home"}>
             <section>
-                <Title title={"Dernier ajout"} />
+                <Title title={"Dernier ajout"}/>
                 <div id={"card-list"}>
-                    { eventArray && eventArray.map((event) => <Card key={event.record.id} event={event}/>) }
+                    {!loading &&
+                    <div className={"loader"}>
+                        <Loader type={"TailSpin"} color="#24376f" height={80} width={80}/>
+                    </div>
+                    }
+                    {eventArray && eventArray.map((event) => <Card key={event.record.id} event={event}/>)}
                 </div>
             </section>
         </div>
--- src/views/SearchList/SearchList.jsx
+++ src/views/SearchList/SearchList.jsx
@@ -3,16 +3,24 @@ import eventService from "../../services/eventService";
 import Card from "../../components/Card/Card";
 import Title from "../../components/Title/Title";
 import {IoIosSearch} from "react-icons/io"
+import Loader from "react-loader-spinner"
 
 const SearchList = () => {
     const inputSearch = useRef();
-    const [searchArray, setSearchArray] = useState([])
+    const [searchArray, setSearchArray] = useState(null)
+    const [loading, setloading] = useState(" ")
+    const [data, setData] = useState(" ")
 
     function searchEvent () {
+        setloading(false)
         const search = inputSearch.current.value
         eventService
             .searchResult(search)
-            .then((data) => setSearchArray(data.records))
+            .then((data) => {
+                setSearchArray(data.records)
+                setloading(true)
+                data.total_count === 0 ? setData(false) : setData(true)
+            })
             .catch((err) => console.log(err))
 
         inputSearch.current.value = ''
@@ -37,6 +45,12 @@ const SearchList = () => {
 
             </header>
             <div id={'card-list'}>
+                {!loading &&
+                <div className={"loader"}>
+                    <Loader type={"TailSpin"} color="#24376f" height={80} width={80}/>
+                </div>
+                }
+                {!data && <p>Aucun événement trouvé pour votre rechercher.</p>}
                 {searchArray && searchArray.map((data) => <Card key={data.record.id} event={data}/>)}
             </div>
         </div>
